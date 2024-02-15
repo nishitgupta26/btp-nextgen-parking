b569d70f17712ef12bee6e159e186f992f9c4e80
vishrutpanya <vishrutpanya03@gmail.com>
2024-02-15 12:02:50 +0530

    create manager-signin route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './pages/Home';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ManagerSignIn from './pages/manager/ManagerSignIn.jsx';
 
 export default function App() {
   return <BrowserRouter>
   <Routes>
     <Route path = "/" element = {<Home />} />
+    <Route path="/manager/sign-in" element={<ManagerSignIn />} />
   </Routes>
   </BrowserRouter>
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,48 +1,47 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  const [selectedRole, setSelectedRole] = useState(null);
+  const navigate = useNavigate();
+
+  const callManagerSignIn = () => {
+    navigate("/manager/sign-in");
+  };
 
   return (
-    <div className="bg-slate-300">
+    <div className="bg-slate-300 min-h-screen flex flex-col">
       <h1 className="text-4xl text-center text-slate-700 font-bold py-8">
         Welcome to our Smart Parking System
       </h1>
-      <div className="flex flex-col justify-center items-center min-h-screen bg-slate-100">
-        <div className="text-4xl font-bold mb-8 text-black">Who are you?</div>
+      <div className="flex flex-col justify-center items-center flex-grow bg-slate-100">
+        <div className="text-4xl font-bold mb-8 text-black">
+          Who are you?
+        </div>
         <div className="flex flex-col space-y-4">
-          <div className="flex flex-col items-centers max-w-lg">
-            <button
-              className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors"
-              onClick={() => setSelectedRole('User')}
-            >
-              User
-            </button>
-          </div>
-          <div className="flex flex-col items-centers max-w-lg">
-            <button
-              className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors"
-              onClick={() => setSelectedRole('Admin')}
-            >
-              Admin
-            </button>
-          </div>
-          <div className="flex flex-col items-centers max-w-lg">
-            <button
-              className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors"
-              onClick={() => setSelectedRole('Owner')}
-            >
-              Owner
-            </button>
-          </div>
-          <div className="flex flex-col items-centers max-w-lg">
-            <button
-              className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors"
-              onClick={() => setSelectedRole('Manager')}
-            >
-              Manager
-            </button>
-          </div>
+          <button
+            className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors w-48"
+            onClick={() => navigate("/user")}
+          >
+            User
+          </button>
+          <button
+            className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors w-48"
+            onClick={() => navigate("/admin")}
+          >
+            Admin
+          </button>
+          <button
+            className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors w-48"
+            onClick={() => navigate("/owner")}
+          >
+            Owner
+          </button>
+          <button
+            className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors w-48"
+            onClick={callManagerSignIn}
+          >
+            Manager
+          </button>
         </div>
       </div>
     </div>
