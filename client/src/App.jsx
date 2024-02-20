7166ec3d610d8983857da18e40909b9a645ced21
Nishit Gupta <gargnishit.2626@gmail.com>
2024-02-20 23:22:19 +0530

    login page basic ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Home from './pages/Home';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './components/Login';
 
 export default function App() {
   return <BrowserRouter>
   <Routes>
-    <Route path = "/" element = {<Home />} />
+    <Route path = "/" element = {<Login />} />
   </Routes>
   </BrowserRouter>
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,44 +1,47 @@
 import React, { useState } from "react";
-
+import "./home.css";
 export default function Home() {
   const [selectedRole, setSelectedRole] = useState(null);
 
   return (
-    <div className="bg-slate-300">
-      <h1 className="text-4xl text-center text-slate-700 font-bold py-8">
-        Welcome to our Smart Parking System
-      </h1>
-      <div className="flex flex-col justify-center items-center min-h-screen bg-slate-100">
+    <div className="min-h-screen flex flex-col">
+      <div className="bg-slate-300">
+        <h1 className="border border-10 border-blue-600 text-4xl text-white text-center font-bold py-8">
+          Welcome to Next-Gen Parking System
+        </h1>
+      </div>
+      <div className="flex-grow flex flex-col justify-center items-center bg-slate-100">
         <div className="text-4xl font-bold mb-8 text-black">Who are you?</div>
         <div className="flex flex-col space-y-4">
           <div className="flex flex-col items-centers max-w-lg">
             <button
-              className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors"
-              onClick={() => setSelectedRole('User')}
+              className="button"
+              onClick={() => setSelectedRole("User")}
             >
               User
             </button>
           </div>
           <div className="flex flex-col items-centers max-w-lg">
             <button
-              className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors"
-              onClick={() => setSelectedRole('Admin')}
+              className="button"
+              onClick={() => setSelectedRole("Admin")}
             >
               Admin
             </button>
           </div>
           <div className="flex flex-col items-centers max-w-lg">
             <button
-              className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors"
-              onClick={() => setSelectedRole('Owner')}
+              className="button"
+              onClick={() => setSelectedRole("Owner")}
             >
               Owner
             </button>
           </div>
           <div className="flex flex-col items-centers max-w-lg">
             <button
-              className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors"
-              onClick={() => setSelectedRole('Manager')}
+              className="button"
+              // className="bg-slate-700 text-white px-4 py-2 rounded-lg uppercase cursor-pointer hover:bg-slate-900 transition-colors"
+              onClick={() => setSelectedRole("Manager")}
             >
               Manager
             </button>
