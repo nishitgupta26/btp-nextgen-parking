6641d6a03a3d5926934059793a956f4bacd1f399
Nishit Gupta <gargnishit.2626@gmail.com>
2024-02-21 01:50:25 +0530

    complete login page frontend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import "./header.css";
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Welcome to</span>
-            <span className='text-slate-700'>SmartPark</span>
+            <span className='text-slate-400'>Welcome to&nbsp;</span>
+            <span className='text-slate-200'>SmartPark</span>
           </h1>
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
@@ -21,17 +22,17 @@ export default function Header() {
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-slate-200 hover:underline'>
               Home
             </li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-slate-200 hover:underline'>
               About
             </li>
           </Link>
           <Link to='/sign-in'>
-            <li className=' text-slate-700 hover:underline'> Employee Sign in</li>
+            <li className=' text-slate-200 hover:underline'> Employee Sign in</li>
           </Link>
 
         </ul>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,146 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./signIn.css";
 export default function SignIn() {
+  const [type, setType] = useState("User");
+
   return (
-    <div>
-      Sign in 
+    <div class="outer">
+      <div class="main">
+        <input type="checkbox" id="chk" aria-hidden="true" />
+
+        <div class="login">
+          <form class="form">
+            <label for="chk" aria-hidden="true">
+              Log in
+            </label>
+            <input
+              class="input"
+              type="email"
+              name="email"
+              placeholder="Email"
+              required=""
+            />
+            <input
+              class="input"
+              type="password"
+              name="pswd"
+              placeholder="Password"
+              required=""
+            />
+            <div class="container">
+              <form>
+                <label>
+                  <input
+                    type="radio"
+                    name="radio"
+                    onChange={(e) => setType("User")}
+                    checked = {type === "User"}
+                  />
+                  <span>User</span>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="radio"
+                    onChange={(e) => setType("Owner")}
+                    checked = {type === "Owner"}
+                  />
+                  <span>Owner</span>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="radio"
+                    onChange={(e) => setType("Manager")}
+                    checked = {type === "Manager"}
+                  />
+                  <span>Manager</span>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="radio"
+                    onChange={(e) => setType("Admin")}
+                    checked = {type === "Admin"}
+                  />
+                  <span>Admin</span>
+                </label>
+              </form>
+            </div>
+            <button>Log in</button>
+          </form>
+        </div>
+
+        <div class="register">
+          <form class="form">
+            <label for="chk" aria-hidden="true">
+              Register
+            </label>
+            <input
+              class="input"
+              type="text"
+              name="txt"
+              placeholder="Username"
+              required=""
+            />
+            <input
+              class="input"
+              type="email"
+              name="email"
+              placeholder="Email"
+              required=""
+            />
+            <input
+              class="input"
+              type="password"
+              name="pswd"
+              placeholder="Password"
+              required=""
+            />
+            <div class="container">
+              <form>
+                <label>
+                  <input
+                    type="radio"
+                    name="radio"
+                    onChange={(e) => setType("User")}
+                    checked = {type === "User"}
+                  />
+                  <span>User</span>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="radio"
+                    onChange={(e) => setType("Owner")}
+                    checked = {type === "Owner"}
+                  />
+                  <span>Owner</span>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="radio"
+                    onChange={(e) => setType("Manager")}
+                    checked = {type === "Manager"}
+                  />
+                  <span>Manager</span>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="radio"
+                    onChange={(e) => setType("Admin")}
+                    checked = {type === "Admin"}
+                  />
+                  <span>Admin</span>
+                </label>
+              </form>
+            </div>
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
