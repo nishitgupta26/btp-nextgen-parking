6551b75b4d5c02fff28988a5dba9cf2c0ceb42a1
Nishit Gupta <gargnishit.2626@gmail.com>
2024-04-03 21:17:59 +0530

    UpdateManagers fixed

--- client/src/pages/UpdateManagers.jsx
+++ client/src/pages/UpdateManagers.jsx
@@ -100,56 +100,66 @@ export default function UpdateManagers() {
 
   return (
     <div className="p-3 max-w-screen min-h-screen mx-auto">
-      {managers.length > 0 ? (
-        <div className="flex flex-col gap-4">
-          <h1 className="text-center mt-7 text-2xl font-semibold">
-            Your Managers
-          </h1>
-          {managers.map((manager) => (
-            <div
-              key={manager._id}
-              className="border rounded-lg p-3 flex justify-between items-center gap-4"
-            >
-              <div className="flex flex-col">
-                <p>{manager.name}</p>
-              </div>
-
-              <div className="flex flex-col item-center">
-                <button
-                  onClick={() => handleManagerDelete(manager._id)}
-                  className="text-red-700 uppercase"
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div className="text-center mt-3">
-          <p className="text-xl text-blue-700 font-semibold">No Managers</p>
-        </div>
-      )}
-
       <div className="mt-10 flex flex-col">
         <div className="text-center mt-3">
-          <p className="text-xl text-blue-700 font-semibold">Add new manager</p>
+          <h1 className="text-3xl font-semibold text-center mb-4">
+            Add new manager
+          </h1>
         </div>
+        <div className="p-7 lg:px-40 flex flex-col gap-4 md:flex-row md:gap-8">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Enter E-Mail of manager"
+            className="border border-slate-700 p-3 rounded-lg w-full md:w-3/4"
+            id="location"
+          />
 
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          type="text"
-          placeholder="emailID of manager"
-          className="border p-3 rounded-lg m-3"
-          id="location"
-        />
+          <button
+            onClick={handleManager}
+            className="p-3 bg-slate-700 text-white w-full md:w-1/4 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            ADD MANAGER
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        {managers.length > 0 ? (
+          <div className="flex flex-col w-full gap-4 px-8 lg:px-20">
+            <h2 className="text-center mt-7 text-2xl font-semibold">
+              Your Managers
+            </h2>
+            <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+              {managers.map((manager) => (
+                <div
+                  key={manager._id}
+                  className="border rounded-lg p-3 flex justify-between items-center gap-4"
+                >
+                  <div className="flex flex-col">
+                    <p>{manager.name}</p>
+                    {/* Need to be added bcz email is only unique attribute */}
+                    {/* <p>{manager.email}</p> */}
+                  </div>
 
-        <button
-          onClick={handleManager}
-          className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80 mt-2"
-        >
-          ADD THIS MANAGER
-        </button>
+                  <div className="flex flex-col item-center">
+                    <button
+                      onClick={() => handleManagerDelete(manager._id)}
+                      className="bg-red-600 text-white p-1 px-2 w-full  mx-2 rounded-lg uppercase text-center hover:opacity-95"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="text-center  mt-3">
+            <p className="text-xl text-blue-700 font-medium">
+              No Manager Added
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
