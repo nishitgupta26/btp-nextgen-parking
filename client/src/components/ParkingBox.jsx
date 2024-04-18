ad9c3466cd0a5ce0ba83d3bb0833d1a7f7bff767
Nishit Gupta <gargnishit.2626@gmail.com>
2024-04-19 01:09:27 +0530

    BookParking fixed

--- client/src/components/ParkingBox.jsx
+++ client/src/components/ParkingBox.jsx
@@ -2,14 +2,12 @@ import React from "react";
 
 export default function ParkingBox({ index, booked }) {
   return (
-    <div className="flex">
-      <div
-        className={`flex justify-center items-center w-10 h-10 rounded-lg border-2 border-slate-400 text-white ${
-          index >= booked ? "bg-green-500" : "bg-red-500"
-        }`}
-      >
-        {index + 1}
-      </div>
+    <div
+      className={`flex justify-center items-center w-10 h-10 rounded-lg border-2 border-slate-400 text-white ${
+        index >= booked ? "bg-green-500" : "bg-red-500"
+      }`}
+    >
+      {index + 1}
     </div>
   );
 }
