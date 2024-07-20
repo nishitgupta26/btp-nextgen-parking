c0ab1122f29b11f318485f54b7f49dd9ed27ab11
Nishit Gupta <gargnishit.2626@gmail.com>
2024-07-21 02:15:02 +0530

    css fixes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,23 +25,26 @@ export default function Header() {
 
   return (
     <header
-      className={`top-0 sticky w-full z-30 bg-white border-y-2 border-slate-300 ${HeaderCSS.head}`}
+      className={`top-0 sticky w-full z-30 bg-white border-y-2 border-slate-300 ${HeaderCSS.head} `}
     >
-      <div className="flex w-full justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="flex w-full justify-between items-center max-w-6xl mx-auto p-3 h-16">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-black">Welcome to&nbsp;</span>
             <span className="text-blue-400">SmartPark</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
-          />
-          <FaSearch className="text-slate-600" />
-        </form>
+        {/* <div className="hidden">
+          <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="bg-transparent focus:outline-none w-24 sm:w-64"
+            />
+            <FaSearch className="text-slate-600" />
+          </form>
+        </div> */}
+        
         <ul className="flex gap-4">
           <Link to="/">
             <li className="hidden sm:inline text-black hover:underline">
